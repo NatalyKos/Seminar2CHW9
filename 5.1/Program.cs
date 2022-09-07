8eebaefadb71cb338d4343a1328e06ac8fb264be
Nataly K <doublejanus4@gmail.com>
2022-09-07 22:24:31 +0300

    5 solved by array

---
+++ 5.1/Program.cs
@@ -0,0 +1,23 @@
+﻿// See Написать программу показывающие первые N чисел, для которых каждое следующее равно сумме двух предыдущих. 
+// Первые два элемента последовательности задаются пользователем
+
+Console.WriteLine("Enter the first number: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter the second number: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+int N = 5;
+FibonacciArray(firstNumber,secondNumber,N,0);
+
+int[] FibonacciArray(int A, int B, int C, int D)
+{
+    int[] array = new int[C];
+    for(D = 0; D < C; D++)
+    {
+        array[D] = A + B;
+        Console.WriteLine(array[D]);
+        int tmp = A + B;
+        A = B;
+        B = tmp;
+    }
+    return array;
+}
